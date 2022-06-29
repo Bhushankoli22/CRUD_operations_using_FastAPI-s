c29fac5b41c6022184708526aa18e2fa7e551ff8
Bhushan Koli <52830840+Bhushankoli22@users.noreply.github.com>
2022-06-29 17:13:45 +0530

    Add files via upload

--- config/db.py
+++ config/db.py
@@ -1,6 +1,14 @@
 from sqlalchemy import create_engine, MetaData
+import pymysql
 
-# engine = create_engine("mysql+pymysql://root@localhost: 3306/test")
-engine = create_engine("sqlite:///name.db")
+engine = create_engine("mysql+pymysql://root@localhost:3306/test")
+# engine = create_engine("sqlite:///test.db")
 conn = engine.connect()
 meta = MetaData()
+
+
+# # database connection
+# connection = pymysql.connect(host="localhost", port=8889, user="root", passwd="root", database="SELECTION_DB")
+# cursor = connection.cursor()
+# # some other statements  with the help of cursor
+# connection.close()
